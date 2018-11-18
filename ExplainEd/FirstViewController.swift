8cd4a9152dcb7e18372603aede2aaac1baba8b88
Robert Brennan <rdbrenna@go.olemiss.edu>
2018-11-18 18:47:41 -0500

    Initial Commit

---
+++ ExplainEd/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ExplainEd
+//
+//  Created by Josef Teska on 19/09/2018.
+//  Copyright © 2018 Josef Teska. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
